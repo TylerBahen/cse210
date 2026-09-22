ad692d419845b4f94bd1e364091e00feb72423f9
Tyler Bahen <tylerbahen@gmail.com>
2026-09-22 12:36:14 -0600

    Did assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("First Name:");
+        string first = Console.ReadLine();
+        Console.WriteLine("Last Name:");
+        string last = Console.ReadLine();
+        Console.WriteLine($"Your name is {last}; {first} {last}.");
     }
 }
